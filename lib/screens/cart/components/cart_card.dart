b571a6164340ff28d92dc04373cdfdc5e0b45ed7
Darkhorse07232020 <darkhorse0723@outlook.com>
2020-11-27 21:26:48 +0300

    create key file and change gradle

--- lib/screens/cart/components/cart_card.dart
+++ lib/screens/cart/components/cart_card.dart
@@ -31,29 +31,31 @@ class CartCard extends StatelessWidget {
           ),
         ),
         SizedBox(width: 20),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              cart.product.title,
-              style: TextStyle(color: Colors.black, fontSize: 16),
-              maxLines: 2,
-            ),
-            SizedBox(height: 10),
-            Text.rich(
-              TextSpan(
-                text: "\$${cart.product.price}",
-                style: TextStyle(
-                    fontWeight: FontWeight.w600, color: kPrimaryColor),
-                children: [
-                  TextSpan(
-                      text: " x${cart.numOfItem}",
-                      style: Theme.of(context).textTheme.bodyText1),
-                ],
+        Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                cart.product.title,
+                style: TextStyle(color: Colors.black, fontSize: 16),
+                maxLines: 2,
               ),
-            )
-          ],
-        )
+              SizedBox(height: 10),
+              Text.rich(
+                TextSpan(
+                  text: "\$${cart.product.price}",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600, color: kPrimaryColor),
+                  children: [
+                    TextSpan(
+                        text: " x${cart.numOfItem}",
+                        style: Theme.of(context).textTheme.bodyText1),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ],
     );
   }
